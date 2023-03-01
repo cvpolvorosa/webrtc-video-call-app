1caf314e5fc79cc9b5ce166b1957450a5d5ab9b1
carl.polvorosa <carl.polvorosa@telusinternational.com>
2023-03-01 09:27:41 -0500

    Added sample for incoming call dialog

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -1,4 +1,8 @@
 import * as wss from "./wss.js"
+import * as constants from "./constants.js"
+import * as ui from "./ui.js"
+
+let connectedUserDetails;
 
 export const sendPreOffer = (callType, calleePersonalCode) => {
     const data = {
@@ -11,5 +15,22 @@ export const sendPreOffer = (callType, calleePersonalCode) => {
 
 
 export const handlePreOffer = (data) => {
+    const { callType, callerSocketId } = data;
+    
+    connectedUserDetails = {
+        socketId: callerSocketId,
+        callType
+    }
 
+    if(callType === constants.callType.CHAT_PERSONAL_CODE || callType === constants.callType.VIDEO_PERSONAL_CODE){
+        ui.showIncomingCallDialog(callType, acceptCallHandler, rejectCallHandler);
+    }
+}
+
+const acceptCallHandler = () => {
+    console.log("call accepted")
+}
+
+const rejectCallHandler = () => {
+    console.log("call rejected")
 }
