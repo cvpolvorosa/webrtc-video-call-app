9eef2a0b568676838059cd1e5cb72f112b1cc138
carl.polvorosa <carl.polvorosa@telusinternational.com>
2023-03-01 08:32:31 -0500

    Added copy to clipboard functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
 import * as store from "./store.js" //imports all exported functions from store.js
 import * as wss from "./wss.js"
 
+//initialization of socket io connection
 const socket = io("/"); //Defines the connection. Alt: io("localhost:3000"); / is used incase we'll deploy it
 wss.registerSocketEvents(socket); //passing the connection to register socket events
+
+
+//register event for personal code copy button
+const personalCodeCopyButton = document.getElementById("personal_code_copy_button");
+personalCodeCopyButton.addEventListener("click", () => { //event listener for when copy button is clicked
+    const personalCode = store.getState().socketId; //gets id directly from store
+    navigator.clipboard && navigator.clipboard.writeText(personalCode); //copies to clipboard
+})
